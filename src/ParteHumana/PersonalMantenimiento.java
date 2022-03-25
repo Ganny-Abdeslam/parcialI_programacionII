a2fa1ea9b1ce7fd1bbc349a447d418d19e8b6db8
Gasser Abdeslam <gannyser@gmail.com>
2022-03-24 23:25:37 -0500

    Completacion de la parte humana

--- src/ParteHumana/PersonalMantenimiento.java
+++ src/ParteHumana/PersonalMantenimiento.java
@@ -1,5 +1,13 @@
 package ParteHumana;
 
-public class PersonalMantenimiento {
+public class PersonalMantenimiento extends Trabajador{
+
+    public PersonalMantenimiento(String nombre, String documento, String puesto) {
+        super(nombre, documento, puesto);
+    }
+
+    public void limpiarSala(){
+        //Despues se rellena
+    }
     
 }
--- src/ParteHumana/Vendedor.java
+++ src/ParteHumana/Vendedor.java
@@ -1,5 +1,13 @@
 package ParteHumana;
 
-public class Vendedor {
+public class Vendedor extends Trabajador {
+
+    public Vendedor(String nombre, String documento, String puesto) {
+        super(nombre, documento, puesto);
+    }
+
+    public void venderBoleto(){
+        //Despues se rellena
+    }
     
 }
